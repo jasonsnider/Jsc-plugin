9e66fffc7ecd7a51414f8370fb454f4b77476604
Jason Snider <root@jasonsnider.com>
2013-10-03 07:31:32 -0500

    Added a method for creating psuedo-random uuids, this should be a little more secure than Cake's native String::uuid() method.

--- Lib/Random.php
+++ Lib/Random.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * A library for crateing a psuedo-random noise
  * 
@@ -153,7 +152,6 @@ class Random {
      * Creates some pseudo random jibberish to be used as a salt value.
      * 
      * @return string
-     * @author Jason D Snider <jason.snider@42viral.org>
      */
     public static function makeSalt() {
         $seed = openssl_random_pseudo_bytes(4096);
@@ -174,4 +172,62 @@ class Random {
 
         return $salt;
     }
+	
+	/**
+	 * Generates a  version 4 compliant psuedo-random UUID.
+	 * 
+	 * @link http://www.php.net/manual/en/function.uniqid.php#88023
+	 * @see http://tools.ietf.org/html/rfc4122#section-4.4
+	 * @see http://en.wikipedia.org/wiki/UUID
+	 *
+	 * @return string A UUID, made up of 32 hex digits and 4 hyphens.
+	 */
+	public static function uuid() {
+        
+		$pr_bits = null;
+		$fp = @fopen('/dev/urandom','rb');
+		if ($fp !== false) {
+			$pr_bits .= @fread($fp, 16);
+			@fclose($fp);
+		} else {
+			// If /dev/urandom isn't available (eg: in non-unix systems), use mt_rand().
+			$pr_bits = "";
+			for($cnt=0; $cnt < 16; $cnt++){
+				$pr_bits .= chr(mt_rand(0, 255));
+			}
+		}
+        
+		$time_low = bin2hex(substr($pr_bits,0, 4));
+		$time_mid = bin2hex(substr($pr_bits,4, 2));
+		$time_hi_and_version = bin2hex(substr($pr_bits,6, 2));
+		$clock_seq_hi_and_reserved = bin2hex(substr($pr_bits,8, 2));
+		$node = bin2hex(substr($pr_bits,10, 6));
+        
+		/**
+		 * Set the four most significant bits (bits 12 through 15) of the
+		 * time_hi_and_version field to the 4-bit version number from
+		 * Section 4.1.3.
+		 * @see http://tools.ietf.org/html/rfc4122#section-4.1.3
+		 */
+        $time_hi_and_version = hexdec($time_hi_and_version);
+        $time_hi_and_version = $time_hi_and_version >> 4;
+        $time_hi_and_version = $time_hi_and_version | 0x4000;
+
+        /**
+         * Set the two most significant bits (bits 6 and 7) of the
+         * clock_seq_hi_and_reserved to zero and one, respectively.
+         */
+        $clock_seq_hi_and_reserved = hexdec($clock_seq_hi_and_reserved);
+        $clock_seq_hi_and_reserved = $clock_seq_hi_and_reserved >> 2;
+        $clock_seq_hi_and_reserved = $clock_seq_hi_and_reserved | 0x8000;
+        
+		return sprintf(
+            '%08s-%04s-%04x-%04x-%012s',
+            $time_low,
+            $time_mid,
+            $time_hi_and_version,
+            $clock_seq_hi_and_reserved,
+            $node
+		);
+    }    
 }
