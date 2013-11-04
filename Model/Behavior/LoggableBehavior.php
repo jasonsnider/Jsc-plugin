6c91bd6d9e437d5b86b57d85c19b96bd9e752106
Jason Snider <root@jasonsnider.com>
2013-11-04 07:23:40 -0600

    Improved code quality

--- Model/Behavior/LoggableBehavior.php
+++ Model/Behavior/LoggableBehavior.php
@@ -3,12 +3,12 @@
  * Adds user tracking to created/modified data
  *
  * Parbake (http://jasonsnider.com/parbake)
- * Copyright 2012, Jason D Snider. (http://jasonsnider.com)
+ * Copyright 2012 - 2013, Jason D Snider. (http://jasonsnider.com)
  *
  * Licensed under The MIT License
  * Redistributions of files must retain the above copyright notice.
  * 
- * @copyright Copyright 2012, Jason D Snider
+ * @copyright Copyright 2012-2013, Jason D Snider
  * @license http://www.opensource.org/licenses/mit-license.php The MIT License
  */
 
